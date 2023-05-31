a953c21dc3f8adf46375eca431d40c48cbc4ea9d
Yazmin02 <ygonzalezm1704@alumno.ipn.mx>
2023-05-31 03:00:01 -0600

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,9 +1,14 @@
 
 import java.util.List;
+import java.util.Stack;
 
 public class Parser {
 
     private final List<Token> tokens;
+    private Stack<String> stack;
+    private String[][] Tabla;
+
+
 
     private final Token identificador = new Token(TipoToken.IDENTIFICADOR, "");
     private final Token select = new Token(TipoToken.SELECT, "select");
@@ -21,7 +26,26 @@ public class Parser {
 
     public Parser(List<Token> tokens){
         this.tokens = tokens;
+        stack=new Stack<>();
+
+        Tabla=new String[][]{
+            {"Q", "Q->Select D From T", "","","","","","","" },
+            {"D", "", "","D->distinctP","D->P","","","D->P",""},
+            {"P", "", "","","P->*","","","P->A",""},
+            {"A", "", "","","","","","A->A1A2",""},
+            {"A1", "", "A1->E","","","A1->,A","","",""},
+            {"A2", "", "","","","","","A2->idA3",""}, 
+            {"A3", "", "A3->E","","","A3->E","A3->id","",""},
+            {"T", "", "","","","","","T->T1T2",""},
+            {"T1", "", "","","","T1->,T","","","T1->E"},
+            {"T2", "", "","","","","","T2->idT3",""},
+            {"T3", "", "","","T3->E","","","T3->id","T3->E"},
+
+        };
     }
+
+    
+
 
     public void parse(){
         i = 0;
@@ -30,6 +54,7 @@ public class Parser {
 
         if(!hayErrores && !preanalisis.equals(finCadena)){
             System.out.println("Error en la posición " + preanalisis.posicion + ". No se esperaba el token " + preanalisis.tipo);
+            
         }
         else if(!hayErrores && preanalisis.equals(finCadena)){
             System.out.println("Consulta válida");
